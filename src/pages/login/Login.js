fc969bb7f0c86bec0a983cdb5782014165274fe6
obital <694767526@qq.com>
2021-09-06 18:33:18 +0800

    更改登录页

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,9 +1,23 @@
 import React from "react";
+import "../../layout/login/login.less";
+
+import { Input, InputNumber } from "antd";
+
+import logo from "../../static/image/logo.png";
 
 class Login extends React.Component {
   render() {
-    return <div>
-      来自login
+    return <div className="login">
+      <div className="container">
+        <div>
+          <img src={logo}/>
+        </div>
+        <div>
+          <Input placeholder="请输入登录账号"/>
+          <Input type="password" placeholder="请输入登录密码"/>
+          <Input placeholder="请输入6位短信验证码"/>
+        </div>
+      </div>
     </div>
   }
 }
